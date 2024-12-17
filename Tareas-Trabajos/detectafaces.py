266251e42aced3ab556e99b9b3c5cb61b9249e05
AngelRamos27 <angeldevs27@gmail.com>
2024-12-16 21:31:59 -0600

    cambio de ruta

--- Tareas-Trabajos/detectafaces.py
+++ Tareas-Trabajos/detectafaces.py
@@ -2,7 +2,7 @@ import numpy as np
 import cv2 as cv
 import math 
 
-object = cv.CascadeClassifier('C:/Users/angel/OneDrive/Escritorio/IA/EjerciciosPy/haarcascade_frontalface_alt.xml')
+object = cv.CascadeClassifier(r'C:\Users\angel\OneDrive\Escritorio\IA\Tareas-Trabajos\haarcascade_frontalface_alt.xml')
 cap = cv.VideoCapture(0)
 i = 0  
 while True:
